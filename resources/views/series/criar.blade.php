c9253285a54e9410b379317208e119e2e9f4bd23
rodrigobispo <rodrigoaugustocb@gmail.com>
2022-04-13 20:44:37 -0300

    Cadastro de série com quantidade de temporadas e episódios

--- resources/views/series/criar.blade.php
+++ resources/views/series/criar.blade.php
@@ -17,11 +17,24 @@ Adicionar Série
 
 <form method="POST">
     @csrf
-    <div class="form-group">
-        <label for="nome" class="input-group">Nome</label>
-        <input type="text" class="form-control" name="nome">
+    <div class="row">
+        <div class="col col-8">
+            <label for="nome">Nome</label>
+            <input type="text" class="form-control" name="nome" id="nome">
+        </div>
+
+        <div class="col col-2">
+            <label for="qtd_temporadas">Nº de Temporadas</label>
+            <input type="number" class="form-control" name="qtd_temporadas" id="qtd_temporadas">
+        </div>
+
+        <div class="col col-2">
+            <label for="ep_por_temporada">Ep. por Temporada</label>
+            <input type="number" class="form-control" name="ep_por_temporada" id="ep_por_temporada">
+        </div>
     </div>
-    <button class="btn btn-primary">Adicionar</button>
+
+    <button class="btn btn-primary mt-2">Adicionar</button>
 </form>
 @endsection
 
